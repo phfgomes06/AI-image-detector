509d51166b13494cbcb22dfcbc8f4b8c3e55655c
Paulo Henrique <phfgomes@outlook.com>
2026-02-07 12:24:08 -0300

    + get_spectrum_image function, that applies fourier transform to the image

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-def analyze_frequency(image_path):
-    img = cv2.imread(image_path, 0) # Carrega em escala de cinza
-    dft = np.fft.fft2(img)          # Transformada de Fourier
+def get_spectrum_image(image_path):
+    img = cv2.imread(image_path, 0)
+    dft = np.fft.fft2(img)
     dft_shift = np.fft.fftshift(dft)
-    magnitude_spectrum = 20 * np.log(np.abs(dft_shift))
-    return magnitude_spectrum
+    magnitude_spectrum = 20 * np.log(np.abs(dft_shift + 1))
+    
+    spectrum_normalized = cv2.normalize(magnitude_spectrum, None, 0, 255, cv2.NORM_MINMAX)
+    spectrum_final = np.uint8(spectrum_normalized)
+
+    return spectrum_final
+
